a4090466a9ad53680494575287f632b0a96939fb
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2022-02-08 12:06:00 +0530

    Create CartView which queries data using the jwt token in the cookies

--- cart/urls.py
+++ cart/urls.py
@@ -2,14 +2,14 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from .views import CategoryViewSet, ProductViewSet, CartViewSet
+from .views import CategoryViewSet, ProductViewSet, CartView
 
 
 router = routers.DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'products', ProductViewSet)
-router.register(r'', CartViewSet, basename='Cart')
 
 urlpatterns = [
-    path('', include((router.urls, 'cart')))
+    path('', include((router.urls, 'cart'))),
+    path('view/', CartView.as_view(), name='cart_view'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,58 +1,41 @@
+import jwt
 from django.contrib.auth import get_user_model
 
-from rest_framework import viewsets, status, permissions
-from rest_framework.decorators import action
+from rest_framework import viewsets
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Category, Product, Cart
 from .serializers import CategorySerializer, ProductSerializer, CartSerializer
-from .helpers import CartHelper
+
 
 User = get_user_model()
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
-
     queryset = Category.objects.all().order_by('id')
     serializer_class = CategorySerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
-
     queryset = Product.objects.all().order_by('category_id')
     serializer_class = ProductSerializer
 
 
-class CartViewSet(viewsets.ModelViewSet):
+class CartView(APIView):
 
-    serializer_class = CartSerializer
-    permission_classes = [
-        permissions.IsAuthenticated,
-    ]
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
 
-    def get_queryset(self):
-        return Cart.objects.all().order_by('-id').filter(user=self.request.user)
-
-    @action(
-        methods=['GET'],
-        detail=False,
-        url_name='checkout',
-        url_path='checkout/<int"pk>/',
-    )
-    def checkout(self, request, *args, **kwargs):
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
 
         try:
-            user = User.objects.get(pk=int(kwargs.get('userId')))
-        except Exception as e:
-            return Response(status=status.HTTP_404_NOT_FOUND,
-                            data={'Error:': str(e)})
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated!')
 
-        cart_helper = CartHelper(user)
-        checkout_details = cart_helper.prepare_cart_for_checkout()
-
-        if not checkout_details:
-            return Response(status=status.HTTP_404_NOT_FOUND,
-                            data={'Error': "Cart is empty!"})
-
-        return Response(status=status.HTTP_200_OK,
-                        data={'checkout_details': checkout_details})
+        cart = Cart.objects.all().filter(user_id__exact=payload['id'])
+        serializer = CartSerializer(cart, many=True)
+        return Response(serializer.data)
